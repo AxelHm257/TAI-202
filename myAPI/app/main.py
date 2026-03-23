f1e8f8c35edacfb82292229fd2cd6317ea2e495a
AxelHm257 <123045383@upq.edu.mx>
2026-03-23 15:36:22 -0600

    Comit Práctica 9 Avance

--- myAPI/app/main.py
+++ myAPI/app/main.py
@@ -1,22 +1,18 @@
+#Importaciones
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.routers.usuario import router as usuario_router
-from app.routers.misc import router as misc_router
+from app.routers import usuario, misc
+from app.data.db import engine, Base
+from app.data import usuarios  # Importamos el módulo para registrar el modelo
+
+# Crear las tablas en la base de datos al arrancar
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Mi API con FastAPI",
     description="Axel Santiago Horta Martínez",
     version="1.0.0"
-
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(usuario_router)
-app.include_router(misc_router)
+# Incluir routers correctamente usando los módulos importados
+app.include_router(usuario.router)
+app.include_router(misc.router)
